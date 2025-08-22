17b501d765daf958555544a2ba17df7d214d9037
MohammadHNdev <hosein.norozi434@gmail.com>
2025-08-22 13:26:10 +0330

    Initial commit - Simple CLI tool

---
+++ __init__.py
@@ -0,0 +1,12 @@
+"""
+Simple Spotify Playlist Extractor
+A basic CLI tool to extract Spotify playlists
+"""
+
+__version__ = "1.0.0"
+__author__ = "MohammadHNdev"
+__email__ = "hosein.norozi434@gmail.com"
+
+from .cli import main
+
+__all__ = ['main']
